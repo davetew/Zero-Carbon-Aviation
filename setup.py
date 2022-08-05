b46123f0fec72dfe9c49112990c91e01286112ac
David Tew <davetew@alum.mit.edu>
2022-08-05 15:55:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-   name='Zero-Carbon-Aviation',
+   name='Zero_Carbon_Aviation',
    version='0.1',
    description='A collection of tools that may be used to assess the value propostion of various aviation propulsion system options',
    author='David Tew',
    author_email='davetew@alum.mit.edu',
    url='https://github.com/davetew/Zero-Carbon-Aviation',
-   packages=setuptools.find_packages("Zero_Carbon_Aviation"),  #same as name
+   packages=setuptools.find_packages(),  #same as name
    install_requires=['numpy', 'matplotlib','pandas','pint','cantera'], #external packages as dependencies
 )
